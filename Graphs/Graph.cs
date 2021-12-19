e2406b31db647114c957e08182831a09a3925d27
Nullean <mr000lean@gmail.com>
2021-12-20 01:04:36 +0400

    added AddVertex method, VerticesCout prop and AreVerticesConnected method

--- Graphs/Graph.cs
+++ Graphs/Graph.cs
@@ -10,5 +10,29 @@ namespace Graphs
     {
         public List<List<double>> Matrix { get; private set; }
 
+        public Graph(int vertisesCout = 0)
+        {
+            Matrix = new List<List<double>>(vertisesCout);
+            for (int i = 0; i < vertisesCout; i++)
+            {
+                Matrix[i] = new List<double>(vertisesCout);
+            }
+        }
+
+        public void AddVertex()
+        {
+            var n = Matrix.Count;
+            foreach (var l in Matrix)
+            {
+                l.Add(0);
+            }
+            Matrix.Add(new List<double>(n + 1));
+        }
+
+        public int VerticesCout => Matrix.Count;
+
+        public bool AreVerticesConnected(int vertex1, int vertex2) => 
+            Matrix[vertex1][vertex2] != 0 || Matrix[vertex2][vertex1] != 0;
+        
     }
 }
