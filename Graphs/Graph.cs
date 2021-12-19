aff0aeb2cbe1a983eca3af551f64069aa92c6b4b
Nullean <mr000lean@gmail.com>
2021-12-20 00:47:22 +0400

    added graph class

---
+++ Graphs/Graph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    internal class Graph
+    {
+        public List<List<double>> Matrix { get; private set; }
+
+    }
+}
