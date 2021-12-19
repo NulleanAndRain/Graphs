08842f184807a6aef1e45211e1c847e2d491ce99
Nullean <mr000lean@gmail.com>
2021-12-20 02:14:07 +0400

    added GraphFileSaver.cs

---
+++ Graphs/GraphFileSaver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    internal class GraphFileSaver
+    {
+        public void SaveAsMatrix(string path, Graph graph)
+        {
+
+        }
+
+        public void SaveAsEdgeList(string path, Graph graph)
+        {
+
+        }
+    }
+}
